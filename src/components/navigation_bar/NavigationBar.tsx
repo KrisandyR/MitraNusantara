4adb443fd494d0104b7bbeca47dc3a8770ea3d3c
KrisandyR <krisandyriver@gmail.com>
2023-03-19 21:32:37 +0700

    First navbar iteration bug fixed

--- src/components/navigation_bar/NavigationBar.tsx
+++ src/components/navigation_bar/NavigationBar.tsx
@@ -1,5 +1,5 @@
 //  Region Import External Lib (e.g React, Reactstrap, etc)
-import React, { Component, Fragment, useState } from "react";
+import React, { Component, Fragment, useEffect, useState } from "react";
 import Image from "react-bootstrap/Image";
 import hamburgericon from "../../assets/hamburger.png";
 
@@ -19,8 +19,36 @@ import hamburgericon from "../../assets/hamburger.png";
 import "./NavigationBar.scss";
 import { Collapse } from "react-bootstrap";
 
+const useWindowSize = () => {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
+};
+
 const NavigationBar = () => {
   const [dropdownIsOpen, setDropdownIsOpen] = useState(false);
+  const windowSize = useWindowSize();
 
   return (
     <Fragment>
@@ -29,31 +57,45 @@ const NavigationBar = () => {
           <span className="navbar-title px-3">Mitra Nusantara</span>
         </div>
 
-        <div className="navbar-options-container px-3">
-          <div className="px-4">Home</div>
-          <div className="px-4">About Us</div>
-          <div className="px-4">Booking</div>
-          <div className="px-4 py-2 mx-2 navbar-login-btn">Login</div>
-        </div>
-
-        <div
-            className="navbar-hamburg-icon p-3 mx-2"
-            onClick={() => {
-              setDropdownIsOpen(!dropdownIsOpen);
-            }}
-          >
-            <Image className="hamburg-img" src={hamburgericon} fluid />
+        {windowSize.width > 1200 && (
+          <div className="navbar-options-container px-3">
+            <div className="px-4">Home</div>
+            <div className="px-4">About Us</div>
+            <div className="px-4">Booking</div>
+            <div className="px-4 py-2 mx-2 navbar-login-btn">Login</div>
           </div>
+        )}
 
-        <Collapse in={dropdownIsOpen}>
-          <div className="navbar-dropdown">
-            <p className="navbar-dropdown-item m-0">Home</p>
-            <p className="navbar-dropdown-item m-0">About Us</p>
-            <p className="navbar-dropdown-item m-0">Booking</p>
-            <p className="navbar-dropdown-item m-0">Login</p>
-          </div>
-        </Collapse>
+        {windowSize.width < 1200 && (
+          <Fragment>
+            <div
+              className="navbar-hamburg-icon p-3 mx-2"
+              onClick={() => {
+                setDropdownIsOpen(!dropdownIsOpen);
+              }}
+            >
+              <Image className="hamburg-img" src={hamburgericon} fluid />
+            </div>
+            <Collapse in={dropdownIsOpen}>
+              <div className="navbar-dropdown-container">
+                <div className="navbar-dropdown">
+                  <p className="navbar-dropdown-item m-0">Home</p>
+                  <p className="navbar-dropdown-item m-0">About Us</p>
+                  <p className="navbar-dropdown-item m-0">Booking</p>
+                  <p className="navbar-dropdown-item m-0">Login</p>
+                </div>
 
+                {/* Outer area for navbar */}
+                {/* <div
+                  className="outer-area-navbar-dropdown"
+                  onClick={() => {
+                    setDropdownIsOpen(!dropdownIsOpen);
+                  }}
+                ></div> */}
+              </div>
+            </Collapse>
+          </Fragment>
+        )}
       </div>
     </Fragment>
   );
